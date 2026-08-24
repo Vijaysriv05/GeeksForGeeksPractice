e657a5d9f48eee22bcc7e2830b810243588952c7
Vijay sri V <vijaysrivenkat2105@gmail.com>
2026-08-24 16:17:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Logical Operators/logical-operators.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String booleanOperations(boolean a, boolean b) {
+        // Code here
+        boolean ans1=a&&b;
+        boolean ans2=a||b;
+        boolean ans3=!a;
+        return ans1+" "+ans2+" "+ans3;
+    }
+}
