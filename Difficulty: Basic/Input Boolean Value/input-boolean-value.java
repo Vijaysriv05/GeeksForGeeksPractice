c32cc49f01740b723ea37dd7971c0cf3819c7541
Vijay sri V <vijaysrivenkat2105@gmail.com>
2026-08-25 15:28:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Boolean Value/input-boolean-value.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean a=sc.nextBoolean();
+          if(a==true){
+              System.out.print(1);
+          }
+          else{
+              System.out.print(0);
+          }
+    }
+}
