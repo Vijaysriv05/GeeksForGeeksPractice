049c501a4b87e34be506ef574ae0d74e23128bc3
Vijay sri V <vijaysrivenkat2105@gmail.com>
2026-08-25 16:25:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Frequency/sorted-frequency.java
@@ -0,0 +1,15 @@
+//Back-end complete function Template for Java
+import java.util.*;
+class Solution {
+    public static void freqSorted(int arr[]) {
+        // Your Code Here
+        Arrays.sort(arr);
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+    }
+}
