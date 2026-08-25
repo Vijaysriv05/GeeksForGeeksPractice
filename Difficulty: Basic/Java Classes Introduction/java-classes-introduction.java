58072c0d1e59c29f9aec9bcebc055d8f4996225f
Vijay sri V <vijaysrivenkat2105@gmail.com>
2026-08-25 15:45:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Classes Introduction/java-classes-introduction.java
@@ -0,0 +1,24 @@
+class task {
+int l;
+int w;
+int h;
+    public void set_length(int l) {
+        // Add your code here.
+        this.l=l;
+    }
+
+    public void set_width(int w) {
+        // Add your code here.
+        this.w=w;
+    }
+
+    public void set_height(int h) {
+        // Add your code here.
+        this.h=h;
+    }
+
+    public void volume() {
+        // Add your code here.
+        System.out.println(l*w*h);
+    }
+}
